e0cc3c8ec8b9e0cc5118a23152323af2407600b6
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-02-20 17:15:03 +0200

    pagination

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -3,7 +3,7 @@ import {axiosService} from "./axios.service";
 import {IMovie, IMovieDetails, IMovieResponse} from "../interfaces";
 
 export const moviesService = {
-    getAll: () => axiosService.get<IMovieResponse>('/discover/movie'),
+    getAll: () => axiosService.get<IMovieResponse>('/discover/movie?&page=1'),
     getMovieById: (movie_id: number) => axiosService.get<IMovieDetails>(`movie/${movie_id}`),
     getMoviesByGenreId: (genreId: number) => axiosService.get<{ results: IMovie[] }>(`/discover/movie?with_genres=${genreId}`).then(value => value.data.results)
 }
