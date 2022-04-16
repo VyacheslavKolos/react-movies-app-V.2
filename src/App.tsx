ec392100dcbdb59cdebeb993d163a92b05adce71
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-04-16 11:21:47 +0300

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Layout, MovieDetailsPage, MoviesList, MoviesListByGenrePage, MoviesListC
 import {Route, Routes} from "react-router-dom";
 import {MovieListByFilmNamePage} from "./components/Pages/MovieListByFilmNamePage/MovieListByFilmNamePage";
 
+
 const App:FC= () => {
     return (
         <div>
--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -5,6 +5,6 @@ import baseURL from "../constants/urls";
 export const axiosService = axios.create({
         baseURL,
         headers:
-            {'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjg4NmZlMjZhMmNkMzNkY2Y1NDQyODM5ZWM0YzhmYSIsInN1YiI6IjYyMDNhOGJmZWEzN2UwMDExOWEyZGVlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IKVHqvmmM236kR_MpqoDHEbF3C-2U3MCL3WtbcljcEs`}
+            {'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}`}
     }
 );
